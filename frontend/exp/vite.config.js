774a2bd9d7f09a54f7fa897090f44abf691b3a24
Anirban Singha <143536290+SinghaAnirban005@users.noreply.github.com>
2024-07-28 20:54:37 +0000

    REmoved proxy

--- frontend/exp/vite.config.js
+++ frontend/exp/vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   server: {
-    proxy: {
-      '/api/v1/users': "https://spend-ex-1.onrender.com",
-      '/api/v1/incomes': "https://spend-ex-1.onrender.com",
-      '/api/v1/expenses': "https://spend-ex-1.onrender.com"
-    }
+    // proxy: {
+    //   '/api/v1/users': "https://spend-ex-1.onrender.com",
+    //   '/api/v1/incomes': "https://spend-ex-1.onrender.com",
+    //   '/api/v1/expenses': "https://spend-ex-1.onrender.com"
+    // }
   },
 
   plugins: [react()],
