9f6a0b2c5b3508646b8678860482bbd97875f728
Anirban Singha <143536290+SinghaAnirban005@users.noreply.github.com>
2024-07-28 20:49:47 +0000

    Back to *

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: "https://spend-ex-2.onrender.com",
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
