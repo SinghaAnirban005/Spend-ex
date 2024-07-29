b871cf9819026eb34bb904cef9c4a962b7968c45
Anirban Singha <143536290+SinghaAnirban005@users.noreply.github.com>
2024-07-29 16:28:39 +0000

    Logging received token

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -7,9 +7,12 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
 
+        console.log("Received token:", token); 
+        
         if (!token) {
             throw new ApiError(401, "Unauthorized request")
         }
+        
     
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
     
